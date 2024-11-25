af04048099154ce862f172e3be7f149708026e79
Luis zardo <luisbloomfieldzardo@gmail.com>
2024-11-25 20:01:00 -0300

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+import s from '../footer/footer.module.scss'
+
+export default function Footer() {
+  return (
+    <footer className={s.footer}>
+      <h2>EU SOU A FOOTER</h2>
+    </footer>
+  )
+}
---
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -0,0 +1,5 @@
+export default function LivroDoados (){
+  return(
+    <h2>EU SOU COMPONENTE LIVROS DOADOS</h2>
+  )
+}
