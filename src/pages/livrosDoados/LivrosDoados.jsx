706b183af1c7629056ce86d90ddd78e1f17d3118
Luis zardo <luisbloomfieldzardo@gmail.com>
2024-12-02 20:05:21 -0300

    adicionado responsivo

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,5 +1,15 @@
-export default function LivroDoados (){
-  return(
-    <h2>EU SOU COMPONENTE LIVROS DOADOS</h2>
+import Livro from '../../assets/livro.png'
+import s from './livros.module.scss'
+export default function LivroDoados() {
+  return (
+      <section className={s.livroDoados}>
+        <h1>Livros Doados</h1>
+        <section className={s.livros}>
+          <img src={Livro} alt="Capa do livro O Protagonista" />
+          <p>O protagonista</p>
+          <p>Susanne Andrade</p>
+          <p>Ficção</p>
+        </section>
+      </section>
   )
 }
